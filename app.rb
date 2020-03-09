6ae6bb8c96f9960a73e8f3c81feef7b0b8bc287c
tiburon <thechanmoon@gmail.com>
2020-03-08 23:14:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get('/name') { "My name is chan" }
+
+    get('/hometown') { "My hometown is seoul korea" }
+
+    get('/favorite-song') { "My favorite song is can't take my eyes off you" }
 end
